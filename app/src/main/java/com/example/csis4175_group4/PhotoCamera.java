f2a440b5efa64499ef59b9d705ac9b2b6ed2efff
Hardawinder <81479894+Hardawinder@users.noreply.github.com>
2021-11-05 00:58:15 +0530

    Update PhotoCamera.java
    Adding intent to open camera

--- app/src/main/java/com/example/csis4175_group4/PhotoCamera.java
+++ app/src/main/java/com/example/csis4175_group4/PhotoCamera.java
@@ -2,13 +2,24 @@ package com.example.csis4175_group4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PhotoCamera extends AppCompatActivity {
+    Button cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_camera);
+        cameraButton=findViewById(R.id.button_capture);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            }
+        });
     }
 }
