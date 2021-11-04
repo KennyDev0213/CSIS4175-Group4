fe88fe614fe88da7ae3b98403bbca7cb35c869da
Hardawinder <81479894+Hardawinder@users.noreply.github.com>
2021-11-05 01:00:22 +0530

    Update PhotoCamera.java
    Intent for photo clicking

--- app/src/main/java/com/example/csis4175_group4/PhotoCamera.java
+++ app/src/main/java/com/example/csis4175_group4/PhotoCamera.java
@@ -8,17 +8,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class PhotoCamera extends AppCompatActivity {
-    Button cameraButton;
+    Button cameraButton,exitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_camera);
         cameraButton=findViewById(R.id.button_capture);
+        exitButton=findViewById(R.id.button_exit);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(cameraIntent);
+            }
+        });
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(PhotoCamera.this,LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
