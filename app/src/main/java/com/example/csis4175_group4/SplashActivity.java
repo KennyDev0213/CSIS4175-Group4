e1e456ce5b74dc361e504d87befd263dbf396eaf
kentlee1026 <93231752+kentlee1026@users.noreply.github.com>
2021-11-10 09:45:51 -0800

    Add files via upload

--- app/src/main/java/com/example/csis4175_group4/SplashActivity.java
+++ app/src/main/java/com/example/csis4175_group4/SplashActivity.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.concurrent.TimeUnit;
 
 public class SplashActivity extends AppCompatActivity {
-
+    FirebaseAuth Auth;
+    FirebaseUser User;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Auth = FirebaseAuth.getInstance();
+        User = Auth.getCurrentUser();
 
         getSupportActionBar().hide();
 
@@ -21,8 +28,16 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent main = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(main);
+                if(User != null){
+                    Intent main = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(main);
+                    finish();
+                }else{
+                    Intent main = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(main);
+                    finish();
+                }
+
             }
         }, 3000);
 
