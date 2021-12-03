47e8d224747e233512b8cff7516bb669bdc498ac
KennyDev0213 <58504944+KennyDev0213@users.noreply.github.com>
2021-12-03 10:24:28 -0800

    Added google services config file to repository for firebase and code cleanup

--- app/src/main/java/com/example/csis4175_group4/utility/Option.java
+++ app/src/main/java/com/example/csis4175_group4/utility/Option.java
@@ -1,16 +1,17 @@
 package com.example.csis4175_group4.utility;
 
 import android.content.Intent;
+import android.graphics.Picture;
 
 public class Option {
     //option class for the main menu
     private String title;
+    private Picture thumb;
     private Intent activity;
 
-    public Option(String title, Intent activity){
+    public Option(String title, Picture thumb, Intent activity){
         this.title = title;
+        this.thumb = thumb;
         this.activity = activity;
     }
-
-
 }
