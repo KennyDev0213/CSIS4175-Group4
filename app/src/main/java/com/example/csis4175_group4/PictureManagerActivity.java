7a234545c38bedd291746c3b6ce8adc7140a4770
Huiroung <daylight529@gmail.com>
2021-12-06 10:39:12 -0800

    r

--- app/src/main/java/com/example/csis4175_group4/PictureManagerActivity.java
+++ app/src/main/java/com/example/csis4175_group4/PictureManagerActivity.java
@@ -1,56 +1,97 @@
 package com.example.csis4175_group4;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import static com.google.android.material.internal.ContextUtils.getActivity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.internal.ContextUtils;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+
 //Code to retrieve photos
-public class PictureManagerActivity extends AppCompatActivity {
-    ImageView image1;
-    
+public class PictureManagerActivity extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_picture_manager);
-        ImageView rImage;
-
-        rImage = findViewById(R.id.image1);
+    int REQUEST_IMAGE_CODE = 1001;
+    int REQUEST_EXTERNAL_STORAGE_PERMISSION = 1002;
+    ImageView imageView;
+    Button btn_load;
 
 
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        View root = inflater.inflate(R.layout.activity_picture_manager, container, false);
 
+        if (ContextCompat.checkSelfPermission( getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)){
 
-        DatabaseReference getImage = databaseReference.child("image");
-
-
-        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                String link = dataSnapshot.getValue(String.class);
-
-
-                Picasso.get().load(link).into(rImage);
+            }
+            else{
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE_PERMISSION);
             }
 
+        } else {
 
+        }
+
+        imageView = root.findViewById(R.id.images);
+        btn_load = root.findViewById(R.id.button);
+
+        btn_load.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                Toast.makeText(PictureManagerActivity.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQUEST_IMAGE_CODE);
             }
         });
+
+        return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CODE){
+            Uri image = data.getData();
+            try{
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), image);
+                imageView.setImageBitmap(bitmap);
+
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
---
+++ app/src/main/java/com/example/csis4175_group4/fragments/photomanager/Photo.java
@@ -0,0 +1,6 @@
+package com.example.csis4175_group4.fragments.photomanager;
+
+import androidx.fragment.app.Fragment;
+
+public class Photo extends Fragment {
+}
