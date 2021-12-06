65b24b0a5243d81be5f981b04f879bdf3dfccd8b
kentlee1026 <93231752+kentlee1026@users.noreply.github.com>
2021-12-06 11:30:16 -0800

    Add files via upload

--- app/src/main/java/com/example/csis4175_group4/SplashActivity.java
+++ app/src/main/java/com/example/csis4175_group4/SplashActivity.java
@@ -1,20 +1,27 @@
 package com.example.csis4175_group4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
-public class
-SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
     FirebaseAuth Auth;
     FirebaseUser User;
+    DatabaseReference Ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,7 @@ SplashActivity extends AppCompatActivity {
 
         Auth = FirebaseAuth.getInstance();
         User = Auth.getCurrentUser();
+        Ref = FirebaseDatabase.getInstance().getReference().child("Users");
 
         getSupportActionBar().hide();
 
@@ -29,18 +37,37 @@ SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //testing purpose, remove!
-                Intent main;
                 if(User != null){
-                    main = new Intent(SplashActivity.this, MainActivity.class);
+//                    Toast.makeText(SplashActivity.this, "first", Toast.LENGTH_SHORT).show();
+                    Ref.child(User.getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.exists()){
+                                Intent main = new Intent(SplashActivity.this, PictureManagerActivity.class);
+                                startActivity(main);
+                                finish();
+                            }else{
+                                Intent main = new Intent(SplashActivity.this, UserProfileActivity.class);
+                                startActivity(main);
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
                 }else{
-                    main = new Intent(SplashActivity.this, LoginActivity.class);
+//                    Toast.makeText(SplashActivity.this, "second", Toast.LENGTH_SHORT).show();
+                    Intent main = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(main);
+                    finish();
                 }
-                startActivity(main);
-                finish();
             }
         }, 3000);
 
-        finish();
+//        finish();
     }
 }
